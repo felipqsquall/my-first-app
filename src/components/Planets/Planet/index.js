6941bcb30c66d58e4c0211ddab4642cf922cb003
Felipe Rosa <felipe.squall@gmail.com>
2022-04-09 18:47:41 -0300

    Primeiro APP React - Até a Aula Renderizando múltiplos elementos no JSX

---
+++ src/components/Planets/Planet/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import GrayImg from "../../shared/gray_img";
+import DescriptionWithLink from "../../shared/description_with_link";
+
+const Planet = (props) => {
+
+  const names = ['a','b','c','d']
+
+  let title;
+  if (props.title_with_underline) {
+    title = (
+      <h4>
+        <u>{props.name}</u>
+      </h4>
+    );
+  } else {
+    title = <h4>{props.name}</h4>;
+  }
+
+  return (
+    <div>
+      {title}
+      <DescriptionWithLink description={props.description} link={props.link} />
+      <GrayImg img_url={props.img_url} gray={props.gray} />
+
+      <h4>Satélites</h4>
+      <ul>
+      {names.map((n) => 
+        <li>Satélite {n}</li>)}
+      </ul>
+      <hr/>
+    </div>
+  );
+};
+
+export default Planet;
