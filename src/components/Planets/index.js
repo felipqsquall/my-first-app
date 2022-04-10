6b108a2eed8029b2d36cc40d08ca0f652777b81c
Felipe Rosa <felipe.squall@gmail.com>
2022-04-10 13:32:22 -0300

    Até o final da correção Refatore usando Hooks

--- src/components/Planets/index.js
+++ src/components/Planets/index.js
@@ -1,64 +1,50 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Planet from "./Planet";
 
-
-async function getPlanets(){
-  let response = await fetch('http://localhost:3000/api/planets.json')
-  let data = await response.json()
+async function getPlanets() {
+  let response = await fetch("http://localhost:3000/api/planets.json");
+  let data = await response.json();
   return data;
 }
 
-class Planets extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      planets: [],
-    };
-  }
+const Planets = () => {
+  const [planets, setPlanets] = useState([]);
 
-  componentDidMount() {
-    getPlanets().then(data => {
-      this.setState(state => ({
-        planets: data['planets']
-      }))
+  useEffect(() => {
+    getPlanets().then((data) => {
+      setPlanets(data['planets'])
     })
-  }
+  }, []);
 
-  removeLast = () => {
-    let new_planets = [...this.state.planets]
-    new_planets.pop()
-    this.setState(state => ({
-      planets: new_planets
-    }))
-  }
+  const removeLast = () => {
+    let new_planets = [...planets];
+    new_planets.pop();
+    setPlanets(new_planets);
+  };
 
-  duplicateLastPlanet = () => {
-    let last_planet = this.state.planets[this.state.planets.length -1]
-    this.setState(state => ({
-      planets: [...this.state.planets, last_planet]
-    }))
-  }
+  const duplicateLastPlanet = () => {
+    let last_planet = planets[planets.length - 1];
+    setPlanets([...planets, last_planet]);
+  };
 
-  render() {
-    return (
-      <Fragment>
-        <h3>Planet List</h3>
-        <button onClick={this.removeLast}>Remove Last</button>
-        <button onClick={this.duplicateLastPlanet}>Duplicate Last</button> 
-        <hr />
-        {this.state.planets.map((planet, index) => (
-          <Planet
-            id={planet.id}
-            name={planet.name}
-            description={planet.description}
-            img_url={planet.img_url}
-            link={planet.link}
-            key={index}
-          />
-        ))}
-      </Fragment>
-    );
-  }
-}
+  return (
+    <Fragment>
+      <h3>Planet List</h3>
+      <button onClick={removeLast}>Remove Last</button>
+      <button onClick={duplicateLastPlanet}>Duplicate Last</button>
+      <hr />
+      {planets.map((planet, index) => (
+        <Planet
+          id={planet.id}
+          name={planet.name}
+          description={planet.description}
+          img_url={planet.img_url}
+          link={planet.link}
+          key={index}
+        />
+      ))}
+    </Fragment>
+  );
+};
 
 export default Planets;
